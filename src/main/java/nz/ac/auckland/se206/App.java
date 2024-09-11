9b7e188a2c29b8ee2fe3ca3e57f8d97cf78b8e90
seeratkawr <sman833@aucklanduni.ac.nz>
2024-09-11 15:49:56 +1200

    deleted files from initial commit that are not needed.

--- src/main/java/nz/ac/auckland/se206/App.java
+++ src/main/java/nz/ac/auckland/se206/App.java
@@ -9,7 +9,6 @@ import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
-import nz.ac.auckland.se206.controllers.ChatController;
 import nz.ac.auckland.se206.speech.FreeTextToSpeech;
 // this is a test comment to test github flows
 
@@ -64,8 +63,8 @@ public class App extends Application {
     FXMLLoader loader = new FXMLLoader(App.class.getResource("/fxml/chat.fxml"));
     Parent root = loader.load();
 
-    ChatController chatController = loader.getController();
-    chatController.setProfession(profession);
+    //ChatController chatController = loader.getController();
+    //chatController.setProfession(profession);
 
     Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
     scene = new Scene(root);
