20121a1e05a50b5089381152ca7af46f0b8521ff
nadiaaskari <nask472@aucklanduni.ac.nz>
2024-09-14 12:17:10 +1200

    when the user clicks on the kitchen on the map, it takes them to the chef suspect

--- src/main/java/nz/ac/auckland/se206/controllers/MapController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/MapController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import nz.ac.auckland.se206.App;
 
 // nz.ac
@@ -17,11 +18,24 @@ public class MapController {
   public void changeBackground(String path) {
     background.setImage(new Image(path));
   }
+
   @FXML
   private void onExitClicked(ActionEvent event) {
     try {
       // Open the crime scene view
       App.openCrimeScene(event);
+    } catch (IOException e) {
+      // Print stack trace for debugging in case of error
+      e.printStackTrace();
+    }
+  }
+
+  @FXML
+  private void onKitchenClicked(MouseEvent event) {
+    try {
+      // Open the kitchen view
+      System.out.println("Kitchen clicked");
+      App.openKitchen(event);
     } catch (IOException e) {
       // Print stack trace for debugging in case of error
       e.printStackTrace();
