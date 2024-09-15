36cc2a8745cd8128bd9ae11c4e450d6b1dfa1035
nadiaaskari <nask472@aucklanduni.ac.nz>
2024-09-15 16:49:57 +1200

    added separate pages to the notebook, with more fxml files

---
+++ src/main/java/nz/ac/auckland/se206/controllers/Notebookpg2Controller.java
@@ -0,0 +1,49 @@
+package nz.ac.auckland.se206.controllers;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import nz.ac.auckland.se206.App;
+
+public class Notebookpg2Controller {
+
+  @FXML private Pane bookPane;
+  @FXML private Pane mainPane;
+
+  @FXML
+  private void onGoLastPage(MouseEvent event) throws IOException {
+    System.out.println("Go last page");
+    App.goLastPage(event);
+  }
+
+  @FXML
+  private void onClickedBook(MouseEvent event) {
+    System.out.println("Book clicked");
+    // if the book is clicked, set the bookpane to be visible
+    bookPane.setVisible(true);
+    mainPane.setVisible(false);
+  }
+
+  @FXML
+  private void onExitBook(ActionEvent event) {
+    System.out.println("Go back");
+    // if the back button is clicked, set the bookpane to be invisible
+    bookPane.setVisible(false);
+    mainPane.setVisible(true);
+  }
+
+  @FXML
+  private void onGoBackCrimeScene(ActionEvent event) throws IOException {
+    System.out.println("Go back to crime scene");
+    // if the back button is clicked, set the bookpane to be invisible
+    App.openCrimeScene(event);
+  }
+
+  @FXML
+  private void onGoFirstPage(MouseEvent event) throws IOException {
+    System.out.println("Go first page");
+    App.GoFirstPage(event);
+  }
+}
