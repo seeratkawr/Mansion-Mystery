6b00c575654054faaec975e28496804ecc0afc92
seeratkawr <sman833@aucklanduni.ac.nz>
2024-09-16 17:49:57 +1200

    finished implementing the chat function for daughter

--- src/main/java/nz/ac/auckland/se206/controllers/DaughterController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/DaughterController.java
@@ -1,14 +1,20 @@
 package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
-
+import java.util.HashMap;
+import java.util.Map;
 import javafx.animation.TranslateTransition;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
 import nz.ac.auckland.apiproxy.chat.openai.ChatCompletionRequest;
 import nz.ac.auckland.apiproxy.chat.openai.ChatCompletionResult;
 import nz.ac.auckland.apiproxy.chat.openai.ChatMessage;
@@ -16,52 +22,46 @@ import nz.ac.auckland.apiproxy.chat.openai.Choice;
 import nz.ac.auckland.apiproxy.config.ApiProxyConfig;
 import nz.ac.auckland.apiproxy.exceptions.ApiProxyException;
 import nz.ac.auckland.se206.App;
+import nz.ac.auckland.se206.prompts.PromptEngineering;
 
 // nz.ac.auckland.se206.controllers.DaughterController
 public class DaughterController {
   @FXML private Button sendButton;
   @FXML private TextField textInput;
   @FXML private TextArea daughterText;
+  @FXML private ImageView loadingIndicator;
 
   private String profession;
   private ChatCompletionRequest chatCompletionRequest;
   private TranslateTransition translateTransition;
 
   public void initialize() {
+    loadingIndicator.setVisible(false);
+    loadingIndicator.setImage(new Image(getClass().getResourceAsStream("/images/bear.png")));
+    translateTransition = new TranslateTransition(Duration.seconds(2), loadingIndicator);
+    translateTransition.setFromX(383);
+    translateTransition.setToX(642);
+    translateTransition.setCycleCount(TranslateTransition.INDEFINITE);
+    translateTransition.setAutoReverse(true);
+    daughterText.setEditable(false);
+    daughterText.setWrapText(true);
+
     setProfession("Daughter");
-  }
-
-  public void setProfession(String profession) {
-    this.profession = profession;
-    clearChat();
-
-    try {
-      ApiProxyConfig config = ApiProxyConfig.readConfig();
-      chatCompletionRequest = 
-        new ChatCompletionRequest(config)
-          .setN(1)
-          .setTemperature(0.2)
-          .setTopP(0.5)
-          .setMaxTokens(100);
-      
-    } catch (ApiProxyException e) {
-      e.printStackTrace();
-    }
   }
 
   @FXML
   private void onMapClicked(MouseEvent event) {
     try {
-      App.openMap(event, "/images/bedroom.png"); 
+      App.openMap(event, "/images/bedroom.png");
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
 
   @FXML
-  private void onSendButton(ActionEvent event) {
+  private void onSendMessage(ActionEvent event) {
     String message = textInput.getText().trim();
-    if(message.isEmpty()) {
+    if (message.isEmpty()) {
       return;
     }
 
@@ -71,11 +71,60 @@ public class DaughterController {
     ChatMessage userMessage = new ChatMessage("user", message);
     appendChatMessage(userMessage);
 
-    
+    loadingIndicator.setVisible(true);
+    translateTransition.play();
+
+    Task<Void> task =
+        new Task<Void>() {
+          @Override
+          protected Void call() throws Exception {
+            ChatMessage response = runGpt(userMessage);
+            Platform.runLater(
+                () -> {
+                  appendChatMessage(response);
+                  loadingIndicator.setVisible(false);
+                  translateTransition.stop();
+                });
+            return null;
+          }
+        };
+
+    new Thread(task).start();
+  }
+
+  public void setProfession(String profession) {
+    this.profession = profession;
+    clearChat();
+
     try {
-      ChatMessage response = runGpt(userMessage);
+      ApiProxyConfig config = ApiProxyConfig.readConfig();
+      chatCompletionRequest =
+          new ChatCompletionRequest(config)
+              .setN(1)
+              .setTemperature(0.2)
+              .setTopP(0.5)
+              .setMaxTokens(100);
+
+      loadingIndicator.setVisible(true);
+      translateTransition.play();
+
+      Task<Void> task =
+          new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+              ChatMessage systemMessage = new ChatMessage("system", getSystemPrompt());
+              ChatMessage response = runGpt(systemMessage);
+              Platform.runLater(
+                  () -> {
+                    appendChatMessage(response);
+                    loadingIndicator.setVisible(false);
+                    translateTransition.stop();
+                  });
+              return null;
+            }
+          };
+      new Thread(task).start();
     } catch (ApiProxyException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
   }
@@ -100,5 +149,19 @@ public class DaughterController {
   private void clearChat() {
     daughterText.clear();
   }
-  
+
+  private String getSystemPrompt() {
+    Map<String, String> map = new HashMap<>();
+    map.put("profession", profession);
+
+    String promptFileName;
+    if ("Daughter".equals(profession)) {
+      promptFileName = "daughter_prompt.txt";
+    } else {
+      throw new IllegalStateException("Unexpected profession: " + profession);
+    }
+
+    String prompt = PromptEngineering.getPrompt(promptFileName, map);
+    return prompt;
+  }
 }
