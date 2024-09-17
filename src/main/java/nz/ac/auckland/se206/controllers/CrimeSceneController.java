8da726f24f0aa8f4523e243a896ed1f9d489737d
Charles Leng-uch <clen678@aucklanduni.ac.nz>
2024-09-17 16:45:45 +1200

    removing an unused import statement

--- src/main/java/nz/ac/auckland/se206/controllers/CrimeSceneController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/CrimeSceneController.java
@@ -5,7 +5,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Tooltip;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
