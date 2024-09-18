09f52bf666afdafb8d7a7eb34cd13e99f451dc3a
nadiaaskari <nask472@aucklanduni.ac.nz>
2024-09-18 21:28:34 +1200

    fixed notebook and game over scene

--- src/main/java/nz/ac/auckland/se206/controllers/Notebookpg2Controller.java
+++ src/main/java/nz/ac/auckland/se206/controllers/Notebookpg2Controller.java
@@ -42,7 +42,7 @@ public class Notebookpg2Controller {
   }
 
   @FXML
-  private void onGoMiddlePage(MouseEvent event) throws IOException {
+  private void onGoFirstPage(MouseEvent event) throws IOException {
     App.playSound("pageflip.mp3");
     System.out.println("Go first page");
     App.goToPage(event, "notebookpg1");
