ef1f07a72362a4df4a64bf4a6d124ded7b462bc4
nadiaaskari <nask472@aucklanduni.ac.nz>
2024-09-18 23:32:32 +1200

    added backstory to the start game scene

--- src/main/java/nz/ac/auckland/se206/controllers/StartGameController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/StartGameController.java
@@ -1,14 +1,22 @@
 package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
+import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
 import nz.ac.auckland.se206.App;
 
 public class StartGameController {
 
   @FXML private Button startButton;
+  @FXML private Label backstory;
+
+  public void initialize() {
+    fadeInBackstory();
+  }
 
   @FXML
   private void onStartGame(ActionEvent event) {
@@ -21,5 +29,23 @@ public class StartGameController {
       // Print stack trace for debugging in case of error
       e.printStackTrace();
     }
+  }
+
+  private void fadeInBackstory() {
+    // Create a new FadeTransition
+    FadeTransition fadeTransition = new FadeTransition();
+
+    // Set the duration of the fade (e.g., 2 seconds)
+    fadeTransition.setDuration(Duration.seconds(3));
+
+    // Set the label you want to fade
+    fadeTransition.setNode(backstory);
+
+    // Set the start and end opacity values
+    fadeTransition.setFromValue(0.0); // fully transparent
+    fadeTransition.setToValue(1.0); // fully opaque
+
+    // Play the transition
+    fadeTransition.play();
   }
 }
